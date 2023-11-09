010c5be2e77feb5d71d70ee0d0668cab608d4a43
Þorgeir Atli <thorgeirka21@tskoli.is>
2023-11-09 09:44:19 +0000

    Fékk modals til að virka

---
+++ hvar-er-tonlist/src/modals/loginmodal.js
@@ -0,0 +1,37 @@
+// LoginModal.js
+import React, { useState } from 'react';
+
+const LoginModal = ({ isOpen, onRequestClose }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement your login logic here
+    console.log(`Logging in with ${username} and ${password}`);
+    // Close the modal after login
+    onRequestClose();
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginModal;
