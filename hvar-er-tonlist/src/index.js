137dc3453c880ba25233174997294c809270da5e
hedinn1 <hedinnha21@tskoli.is>
2023-11-14 12:56:55 +0000

    material ui test

--- hvar-er-tonlist/src/index.js
+++ hvar-er-tonlist/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,5 +14,3 @@ root.render(
     <App />
   </BrowserRouter>
 );
-
-// test
--- hvar-er-tonlist/src/routes/Events.js
+++ hvar-er-tonlist/src/routes/Events.js
@@ -1,8 +1,9 @@
 // Events.js
 import React from 'react';
+import Button from '@mui/material/Button';
 
 const Events = () => {
-  return <div>Events Page</div>;
+  return <><h1>Viðburðir</h1><Button variant="contained">Hello world</Button>;</>;
 };
 
 export default Events;
