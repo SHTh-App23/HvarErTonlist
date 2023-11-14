3f3fc1513436705e40207865858114a91efeb943
SzymonKZ <4360szymon@gmail.com>
2023-11-14 14:14:39 +0000

    dd

--- hvar-er-tonlist/src/routes/Events.js
+++ hvar-er-tonlist/src/routes/Events.js
@@ -1,10 +1,3 @@
-// Events.js
-
-import React from 'react';
-import Button from '@mui/material/Button';
-
-const Events = () => {
-  return <><h1>Viðburðir</h1><Button variant="contained">Hello world</Button>;</>;
 import React, { useEffect, useState } from 'react';
 
 import axios from 'axios';
