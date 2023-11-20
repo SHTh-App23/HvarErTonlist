5211608b1b8bf7f3831577861d12929f990c5b7b
hedinn1 <hedinnha21@tskoli.is>
2023-11-20 12:03:48 +0000

    ok

--- hvar-er-tonlist/src/components/Navbar.js
+++ hvar-er-tonlist/src/components/Navbar.js
@@ -3,20 +3,21 @@ import logo from './logo.svg'
 
 const Navbar = ({ openLoginModal, openLeitModal, openVidburdurModal }) => {
 
+
+
   return (
     <header>
       <div>
-        <div>
-          <img src={logo} alt="Logo" className='logo' />
-        </div>
-        <div className='flex gap-small'>
-          <button onClick={openVidburdurModal} color="inherit">Nýr viðburður</button>
-          <button onClick={openLeitModal} color="inherit">Leita</button>
-          <button onClick={openLoginModal} color="inherit">Skrá inn</button>
-        </div>
+      <div>
+        <img src={logo} alt="Logo" className='logo' />
+      </div>
+      <div className='flex gap-small'>
+        <button onClick={openVidburdurModal} color="inherit">Nýr viðburður</button>
+        <button onClick={openLeitModal} color="inherit">Leita</button>
+        <button onClick={openLoginModal} color="inherit">Skrá inn</button>
+      </div>
       </div>
     </header>
   );
 };
-
 export default Navbar;
