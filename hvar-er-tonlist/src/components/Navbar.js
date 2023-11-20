a1ef155a1128675bc996732660dbb141ff4ce231
hedinn1 <hedinnha21@tskoli.is>
2023-11-20 09:26:44 +0000

    logo

--- hvar-er-tonlist/src/components/Navbar.js
+++ hvar-er-tonlist/src/components/Navbar.js
@@ -2,29 +2,30 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import logo from './logo.png'
 
 
-const Navbar = ( {openLoginModal, openLeitModal, openVidburdurModal }) => {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar>
-            <Toolbar>
-              <Typography  sx={{ flexGrow: 1 }}>
-                Hvar er tónlist?
-              </Typography>
-              <Link to="/">Home</Link>
-              <Link to="/event">Event</Link>
-              <Link to="/profile">Profile</Link>
-              <Button onClick={openVidburdurModal} color="inherit">Nýr viðburður</Button>
-              <Button onClick={openLeitModal} color="inherit">Leita</Button>
-              <Button onClick={openLoginModal} color="inherit">Skrá inn</Button>
-            </Toolbar>
-          </AppBar>
-        </Box> 
-      );
-  };
+const Navbar = ({ openLoginModal, openLeitModal, openVidburdurModal }) => {
+
+
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar>
+        <Toolbar>
+          <img src={logo} alt="Logo" style={{ height: 50}} />
+          <Link to="/">Home</Link>
+          <Link to="/event">Event</Link>
+          <Link to="/profile">Profile</Link>
+          <Button onClick={openVidburdurModal} color="inherit">Nýr viðburður</Button>
+          <Button onClick={openLeitModal} color="inherit">Leita</Button>
+          <Button onClick={openLoginModal} color="inherit">Skrá inn</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
 export default Navbar;
