56b978e93799469b8c306bd0e7af594a6bc9b15b
hedinn1 <hedinnha21@tskoli.is>
2023-11-20 11:59:32 +0000

    styling

--- hvar-er-tonlist/src/components/Navbar.js
+++ hvar-er-tonlist/src/components/Navbar.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
-import logo from './logo.png'
+import logo from './logo.svg'
 
 
 const Navbar = ({ openLoginModal, openLeitModal, openVidburdurModal }) => {
@@ -12,10 +7,26 @@ const Navbar = ({ openLoginModal, openLeitModal, openVidburdurModal }) => {
 
 
   return (
+    <header>
+      <div>
+      <div>
+        <img src={logo} alt="Logo" className='logo' />
+      </div>
+      <div className='flex gap-small'>
+        <button onClick={openVidburdurModal} color="inherit">Nýr viðburður</button>
+        <button onClick={openLeitModal} color="inherit">Leita</button>
+        <button onClick={openLoginModal} color="inherit">Skrá inn</button>
+      </div>
+      </div>
+    </header>
+    /*
     <Box sx={{ flexGrow: 1 }}>
       <AppBar>
         <Toolbar>
-          <img src={logo} alt="Logo" style={{ height: 50}} />
+          <Typography sx={{ flexGrow: 1 }}>
+          <img src={logo} alt="Logo" style={{ height: 40}} /> 
+          </Typography>
+          
           <Link to="/">Home</Link>
           <Link to="/event">Event</Link>
           <Link to="/profile">Profile</Link>
@@ -25,6 +36,7 @@ const Navbar = ({ openLoginModal, openLeitModal, openVidburdurModal }) => {
         </Toolbar>
       </AppBar>
     </Box>
+    */
   );
 };
 
--- hvar-er-tonlist/src/routes/Home.js
+++ hvar-er-tonlist/src/routes/Home.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-
-
 const Home = () => {
 
   const [events, setEvents] = useState([])
@@ -14,15 +12,31 @@ const Home = () => {
   }, [])
 
   return (
-    <div>
-      <h1>Event List</h1>
-      <ul>
-        {events.map(event => (
-          <li key={event.id}>
-            <Link to={`/event/${event.name}`}>{event.name}</Link>
-          </li>
-        ))}
-      </ul>
+
+    <div className='grid eventsContainer gap-large' >
+      {events.map(event => (
+        <Link key={event._id} to={`/event/${event.name}`}>
+          <div  >
+            <div className='event-img-container'>
+              <img src='https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?w=1380&t=st=1700480482~exp=1700481082~hmac=c3f321efc1f2497f49b8dbed4a4612bd0778c19433f06ef21bcecf29d141869d' />
+            </div>
+            <div className='event-details'>
+              <h3 className='weight-bold'>
+                {event.name}
+              </h3>
+              <h4 className='weight-light'>
+                {event.date}
+              </h4>
+              <h4 className='weight-regular'>
+                {event.location}
+              </h4>
+              <h4 className='weight-bold'>
+                {event.verd}
+              </h4>
+            </div>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
